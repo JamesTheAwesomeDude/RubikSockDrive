de6a200b7194bcfeacba5a23586f835591b8aa90
James Edington <james.edington@uah.edu>
2023-07-08 15:59:09 -0500

    bit of cleanup

--- Tesseract/macaulay.py
+++ Tesseract/macaulay.py
@@ -5,50 +5,7 @@ from . import Multiset
 from ._util import search_maxsatisfying as _search_maxsatisfying, fit_to_epoch as _fit_to_epoch
 
 
-def multicomb(n, k):
-	"https://en.wikipedia.org/wiki/Multiset#Counting_multisets"
-	return _comb(n + k - 1, k)
-
-
-# Set-Multiset bijection
-
-def combination_to_multiset(s):
-	return Multiset(elem - offset for offset, elem in enumerate(sorted(s)))
-
-def multiset_to_combination(ms):
-	return set(elem + offset for offset, elem in enumerate(sorted(ms)))
-
-
-# Multiset-Integer bijections
-
-def integer_to_varmultiset(i, n):
-	k, offset = _fit_to_epoch(i, lambda k: multicomb(n, k))
-	return integer_to_multiset(i - offset, k)
-
-def varmultiset_to_integer(ms, n):
-	k = len(ms)
-	offset = sum(multicomb(n, k) for k in range(k))
-	return multiset_to_integer(ms) + offset
-
-
-def multiset_to_integer(ms):
-	return combination_to_integer(multiset_to_combination(ms))
-
-def integer_to_multiset(i, k):
-	return combination_to_multiset(integer_to_combination(i, k))
-
-
 # Integer-Combination bijection
-
-def integer_to_varcombination(i, n):
-	k, epoch_offset = _fit_to_epoch(i, lambda k: _comb(n, k))
-	return integer_to_combination(i - epoch_offset, k)
-
-def varcombination_to_integer(i, n):
-	k = len(ms)
-	epoch_offset = sum(_comb(n, k) for k in range(k))
-	return combination_to_integer(ms) + epoch_offset
-
 
 def integer_to_combination(i, k):
 	"https://en.wikipedia.org/wiki/Combinatorial_number_system#Finding_the_k-combination_for_a_given_number"
@@ -66,6 +23,57 @@ def integer_to_combination(i, k):
 	assert remainder == 0, {'i': i, 'k': k}
 	return s
 
+
 def combination_to_integer(s):
 	"https://en.wikipedia.org/wiki/Combinatorial_number_system#Place_of_a_combination_in_the_ordering"
 	return sum(_comb(elem, j) for j, elem in enumerate(sorted(s), start=1))
+
+
+# Set-Multiset bijection
+
+def combination_to_multiset(s):
+	return Multiset(elem - offset for offset, elem in enumerate(sorted(s)))
+
+
+def multiset_to_combination(ms):
+	return set(elem + offset for offset, elem in enumerate(sorted(ms)))
+
+
+def multicomb(n, k):
+	"https://en.wikipedia.org/wiki/Multiset#Counting_multisets"
+	return _comb(n + k - 1, k)
+
+
+# Multiset-Integer bijections
+
+def multiset_to_integer(ms):
+	return combination_to_integer(multiset_to_combination(ms))
+
+
+def integer_to_multiset(i, k):
+	return combination_to_multiset(integer_to_combination(i, k))
+
+
+# Modified Multiset- & Combination-Integer bijections
+
+def integer_to_varmultiset(i, n):
+	k, offset = _fit_to_epoch(i, lambda k: multicomb(n, k))
+	return integer_to_multiset(i - offset, k)
+
+def varmultiset_to_integer(ms, n):
+	k = len(ms)
+	offset = sum(multicomb(n, k) for k in range(k))
+	return multiset_to_integer(ms) + offset
+
+
+def integer_to_varcombination(i, n):
+	k, epoch_offset = _fit_to_epoch(i, lambda k: _comb(n, k))
+	return integer_to_combination(i - epoch_offset, k)
+
+
+def varcombination_to_integer(i, n):
+	k = len(ms)
+	epoch_offset = sum(_comb(n, k) for k in range(k))
+	return combination_to_integer(ms) + epoch_offset
+
+
